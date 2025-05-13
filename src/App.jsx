2ecb80dc0a17747ed107837c7e35ea6d3479cb2e
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-13 11:34:13 +0100

    introduce styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -6,22 +9,6 @@ const H1 = styled.h1`
   background-color: yellow;
 `;
 
-const Button = styled.button`
-  font-size: 1.4em;
-  padding: 1.2em 1.6em;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  margin: 20px;
-  cursor: pointer;
-`;
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8em 1.2em;
-`;
 const StyledApp = styled.div`
   background-color: orange;
   padding: 2em;
@@ -29,12 +16,15 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert("hello")}>Check in</Button>
-      <Button onClick={() => alert("hello")}>Check out</Button>
-      <Input type="number" placeholder="Number of Guests" />
-    </StyledApp>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("hello")}>Check in</Button>
+        <Button onClick={() => alert("hello")}>Check out</Button>
+        <Input type="number" placeholder="Number of Guests" />
+      </StyledApp>
+    </>
   );
 }
 
