8c1d377e92a2a0aa232b5c0c6b67706101e4c8e0
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-13 15:21:59 +0100

    add supabase key

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://meaplrfqojmlmpikpous.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lYXBscmZxb2ptbG1waWtwb3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNDA5NDksImV4cCI6MjA2MjcxNjk0OX0.yWBvGo6vaNIPglSGoPJudURos2y420C2dQ9rjk4z-_Y";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
