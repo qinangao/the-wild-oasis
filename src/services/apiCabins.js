9be9966f6f26702b67236092a1ad054035f7715b
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-14 14:26:42 +0100

    fix: correct error class instantiation in getCabins function

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -3,7 +3,7 @@ export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
     console.error(error);
-    throw new error("Cabins could not be loaded");
+    throw new Error("Cabins could not be loaded");
   }
   return data;
 }
