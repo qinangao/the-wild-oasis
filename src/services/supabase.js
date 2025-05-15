c6ce309879b1bbe8b442c15446f40725e3c5ae0c
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-15 14:03:57 +0100

    export supabase url

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://meaplrfqojmlmpikpous.supabase.co";
+export const supabaseUrl = "https://meaplrfqojmlmpikpous.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lYXBscmZxb2ptbG1waWtwb3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNDA5NDksImV4cCI6MjA2MjcxNjk0OX0.yWBvGo6vaNIPglSGoPJudURos2y420C2dQ9rjk4z-_Y";
 const supabase = createClient(supabaseUrl, supabaseKey);
