ee0669e50eb3eb793b322b25e48790cd0556776e
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-16 22:47:48 +0100

    add useOverlayClick hook to manage modal close functionality on overlay click

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -3,6 +3,7 @@ import { createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOverlayClick } from "../hooks/useOverlayClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -75,11 +76,24 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const { ref } = useOverlayClick(close);
+
+  // const ref = useRef();
+
+  // useEffect(() => {
+  //   function handleClick(e) {
+  //     if (ref.current && !ref.current.contains(e.target)) close();
+  //   }
+  //   document.addEventListener("click", handleClick, true);
+
+  //   return () => document.removeEventListener("click", handleClick, true);
+  // }, [close]);
+
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
