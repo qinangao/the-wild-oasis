eeafda8664907e2e47af18f59e66b4d67d9995bd
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-16 22:46:57 +0100

    add overlay click functionality

---
+++ src/hooks/useOverlayClick.js
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from "react";
+
+export function useOverlayClick(handler) {
+  const ref = useRef();
+  useEffect(() => {
+    function handleClick(e) {
+      if (ref.current && !ref.current.contains(e.target)) handler();
+    }
+    document.addEventListener("click", handleClick, true);
+
+    return () => document.removeEventListener("click", handleClick, true);
+  }, [handler]);
+
+  return { ref };
+}
