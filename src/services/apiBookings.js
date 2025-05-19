f0b3590e174245200e4a4e99bcdd1c71a66d37a2
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-19 13:15:28 +0100

    feat: add filtering capability to getAllBookings function

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,15 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getAllBookings() {
-  let { data, error } = await supabase
+export async function getAllBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id,created_at,startDate,endDate,numNights,numGuests, status,totalPrice, cabins(name),guests(fullName,email)"
     );
+  if (filter !== null)
+    query = query[filter.method || "eq"](filter.field, filter.value);
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
