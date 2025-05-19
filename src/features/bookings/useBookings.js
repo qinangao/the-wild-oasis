b0976d34ab6f5268a3ecb9480650476a7b1c7e7f
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-19 13:15:11 +0100

    feat: implement filtering for bookings based on status from search parameters

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  //Filter:
+  const filteredValue = searchParams.get("status");
+  const filter =
+    !filteredValue || filteredValue === "all"
+      ? null
+      : {
+          field: "status",
+          value: filteredValue,
+          method: "eq",
+        };
   const {
     isPending,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getAllBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getAllBookings({ filter }),
   });
   return { isPending, bookings, error };
 }
