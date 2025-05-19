ee878d54bc42306b94f38f2e1ce547ed2cf02eae
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-19 11:40:53 +0100

    get booking data from useBookings hook and impletment in Bookings

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,12 +1,19 @@
 import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
+import MenuList from "../../ui/MenuList";
+import Empty from "../../ui/Empty";
+import { useBookings } from "./useBookings";
+import Spinner from "../../ui/Spinner";
 
 function BookingTable() {
-  const bookings = [];
+  const { bookings, isPending } = useBookings();
+
+  if (isPending) return <Spinner />;
+
+  if (!bookings.length) return <Empty resourceName="bookings" />;
 
   return (
-    <Menus>
+    <MenuList>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
         <Table.Header>
           <div>Cabin</div>
@@ -24,7 +31,7 @@ function BookingTable() {
           )}
         />
       </Table>
-    </Menus>
+    </MenuList>
   );
 }
 
