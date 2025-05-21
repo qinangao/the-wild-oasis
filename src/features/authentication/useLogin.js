3d03a2f4c465e352b77fbc0ee870f450df0f72bb
qinanagao@gmail.com <qinanagao@gmail.com>
2025-05-21 14:52:55 +0100

    feat: update useLogin hook to include user data on successful login

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginAPI } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  //   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isPending } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      //   queryClient.setQueryData(["user"], user);
       navigate("/dashboard");
     },
     onError: (err) => {
